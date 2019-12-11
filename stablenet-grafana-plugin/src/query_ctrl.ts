c4067257d4ab4f9d280ae1bb1db5e5f4d80bd5c3
Vasil Alistarov <v.alistarov@gmail.com>
2019-12-11 17:56:39 +0200

    SN-7938: adapted to DH's suggestions

--- stablenet-grafana-plugin/src/query_ctrl.ts
+++ stablenet-grafana-plugin/src/query_ctrl.ts
@@ -76,8 +76,16 @@ export class GenericDatasourceQueryCtrl extends QueryCtrl {
     }
 
     onMeasurementChange() {
-        this.datasource.findMetricsForMeasurement(this.target.selectedMeasurement, this.target.refId).then(res => this.target.metrics = res);
+        this.datasource.findMetricsForMeasurement(this.target.selectedMeasurement, this.target.refId)
+                        .then(res => this.target.metrics = res);
         this.target.chosenMetrics = {};
+        //We need the name of the Measurement with this id, but it is not saved on our side except somewhere in the dropdown box
+        //which is basically an ul with one li per measurement and a small hell to work with. 1.5h wasted.
+        this.datasource.findMeasurementsForDevice(this.target.selectedDevice, this.target.measurementQuery, this.target.refId)
+                                .then(r => r.data)
+                                .then(r => r.filter(m => m.value === this.target.selectedMeasurement)[0])
+                                .then(r => this.target.metricPrefix = r.text)
+        //this.target.metricPrefix = this.target.selectedMeasurement; //@TODO: use name, not obid
         this.onChangeInternal();
     }
 
