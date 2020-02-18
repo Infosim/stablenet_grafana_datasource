1b807243c7f2d430de806b28a5b97ac0d9131f76
Vasil Alistarov <v.alistarov@gmail.com>
2020-02-18 18:15:23 +0100

    SN-8359: minimal non-working example

--- stablenet-grafana-typescript/spec/tests/AssemblerSpec.ts
+++ stablenet-grafana-typescript/spec/tests/AssemblerSpec.ts
@@ -1,8 +1,25 @@
-import {TextValue} from "../../src/returnTypes";
+import {WrappedTarget} from "../../src/data_query_assembler";
 
 describe("A suite", function() {
+    let s: WrappedTarget = new WrappedTarget({
+        datasource: undefined,
+        refId: "A",
+        mode: 10,
+        deviceQuery: 'string',
+        selectedDevice: 0,
+        measurementQuery: 'string',
+        selectedMeasurement: 0,
+        chosenMetrics: [],
+        metricPrefix: 'string',
+        includeMinStats: false,
+        includeAvgStats: false,
+        includeMaxStats: false,
+        statisticLink: '',
+        metrics: [],
+        moreDevices: false,
+        moreMeasurements: false
+    },0);
     it("contains spec with an expectation", function() {
-        let a: TextValue = {text: "a", value: 0};
-        expect(a).not.toEqual({text:"b", value: 1});
+        expect(s).not.toBeNull();
     });
 });
