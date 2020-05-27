9588752cb36c5d5587f18d5f6d411817d0a5eb2d
Vasil Alistarov <vasil.alistarov@stud-mail.uni-wuerzburg.de>
2020-05-27 16:40:43 +0200

    SN-8600: copyright

--- stablenet-grafana-react-700/src/components/CustomAverage.tsx
+++ stablenet-grafana-react-700/src/components/CustomAverage.tsx
@@ -1,3 +1,10 @@
+/*
+ * Copyright: Infosim GmbH & Co. KG Copyright (c) 2000-2020
+ * Company: Infosim GmbH & Co. KG,
+ *                  Landsteinerstraße 4,
+ *                  97074 Wuerzburg, Germany
+ *                  www.infosim.net
+ */
 import React from 'react';
 import { Checkbox, Input, Select, LegacyForms } from '@grafana/ui';
 
